6735009b4489a21ef70006fcc662edee5908fd84
Sandhi Sayem <Sandhi.Sayem@Gmail.com>
2024-02-18 11:45:43 -0500

    Chapter 8, Step 16

--- src/main/java/com/sayem/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/sayem/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,9 +1,14 @@
 package com.sayem.rest.webservices.restfulwebservices.exception;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -26,5 +31,27 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
 		
 		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		
+		List<String> message = new ArrayList<String>();
+		
+//		StringBuffer message = new StringBuffer();
+		ex.getFieldErrors().forEach(m -> {
+//				message.append(m.getDefaultMessage());
+//				message.append(". ");
+				message.add(m.getDefaultMessage());
+			}
+		);
+		
+		
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), String.join(". ", message), request.getDescription(false));
+//		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+		
+		return new ResponseEntity<Object>(errorDetails, HttpStatus.BAD_REQUEST);
+
 	}
 }
